18dc299a31d162686673f2c27e82b80d97a2ce2f
741PC-35\student <cu.common@gmail.com>
2019-07-25 10:35:17 +0900

    仮2画面(作成中)

--- CUFightingMaster/Assets/Scripts/CharacterSelect/CharacterSelectManager.cs
+++ CUFightingMaster/Assets/Scripts/CharacterSelect/CharacterSelectManager.cs
@@ -2,76 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class CharacterSelectManager : MonoBehaviour
 {
-    public int currentSelectNumber = 0;     //0が左
+	public int currentSelectNumber = 0;     //0が左
 
-    public GameObject[] Character = new GameObject[6];
+	private const int maxChara = 6;
+	public GameObject[] character = new GameObject[maxChara];               //キャラクターゲームオブジェクト
+	public GameObject[] characterPanels = new GameObject[maxChara];    //キャラクターパネル
+	public GameObject currentSellectCharacter = null;
+	private GameObject currentSellectPanel;
+	public GameObject selectCursor;     //カーソル
 
-    public GameObject SelectCursor;
-    public GameObject[] CharacterPanels = new GameObject[6];
+	void Start()
+	{
+		CreateCharacter();
+	}
 
-    private GameObject currentSellectPanel;
-    public GameObject currentSellectCharacter;
+	void Update()
+	{
+		//カーソル左移動
+		if (Input.GetKeyDown("a"))
+		{
+			SelectChara(0);
+		}
+		//カーソル右移動
+		else if (Input.GetKeyDown("s"))
+		{
+			SelectChara(1);
+		}
+		//決定(シーン移動)
+		else if (Input.GetKeyDown("b"))
+		{
+			SceneManager.LoadScene("Battle");
+		}
+		CreateCharacter();
+	}
+	public void SelectChara(int _dir)
+	{
+		//カーソルの移動を判定
+		if (_dir == 0)
+		{
+			if (currentSelectNumber > 0) currentSelectNumber--;
+			else currentSelectNumber = 5;
+		}
+		else
+		{
+			if (currentSelectNumber < 5) currentSelectNumber++;
+			else currentSelectNumber = 0;
+		}
+		selectCursor.transform.position = characterPanels[currentSelectNumber].transform.position;
+		CreateCharacter();
+	}
 
-    public GameObject CharacterCreatePoint;
-
-    public void  SelectLeft()
-    {
-        if (currentSelectNumber > 0)
-        {
-            currentSelectNumber--;
-        }
-        else
-            currentSelectNumber = 5;
-
-        SelectCursor.transform.position = CharacterPanels[currentSelectNumber].transform.position;
-
-        CreateCharacter();
-
-    }
-
-    public void SelectRight()
-    {
-        if (currentSelectNumber < 5)
-        {
-            currentSelectNumber++;
-        }
-        else
-            currentSelectNumber = 0;
-
-        SelectCursor.transform.position = CharacterPanels[currentSelectNumber].transform.position;
-
-        CreateCharacter();
-
-    }
-
-    public void DecideCharacter()
-    {
-
-    }
-
-    public void CreateCharacter()
-    {
-        //if (currentSellectCharacter != null)
-        //    Destroy(currentSellectCharacter);
-        //currentSellectCharacter  = Instantiate(Character, CharacterCreatePoint.transform);
-        //currentSellectCharacter.transform.localScale =new Vector3(4, 4, 4);
-
-        currentSellectCharacter.SetActive(false);
-        currentSellectCharacter =  Character[currentSelectNumber];
-        currentSellectCharacter.SetActive(true);
-
-    }
-
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-    
-    }
+	//表示するキャラクターを生成
+	public void CreateCharacter()
+	{
+		if (currentSellectCharacter != null) Destroy(currentSellectCharacter);
+		currentSellectCharacter = Instantiate(character[currentSelectNumber]);
+	}
 }
