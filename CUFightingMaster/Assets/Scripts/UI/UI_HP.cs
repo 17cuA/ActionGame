a596f1394b4bbabaeda7db5a26c001fe64443eca
741PC-34\student <cu.common@gmail.com>
2019-07-30 14:49:19 +0900

    HPバー作成途中

--- CUFightingMaster/Assets/Scripts/UI/UI_HP.cs
+++ CUFightingMaster/Assets/Scripts/UI/UI_HP.cs
@@ -10,11 +10,11 @@
 // 仕様
 // 子オブジェクトに必要なもの
 // ・Mask
-// ・1:ダメージを受けたときにHpバーの色が変わるやつ
-// ・2:Hpバーの緑のところ
+// ・5:ダメージを受けたときにHpバーの色が変わるやつ
+// ・4:Hpバーの緑のところ
 // ・3:ブロックした時に色が変わるところ
-// ・4:ダメージの赤
-// ・5:一番後ろのbackground
+// ・2:ダメージの赤
+// ・1:一番後ろのbackground
 //----------------------------------------
 // MEMO
 // 参照は手動です
@@ -24,4 +24,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public 
+public class UI_HP : MonoBehaviour
+{
+	public GameObject[] hpObjects = new GameObject[5];
+
+	public float hp = 100.0f; //debug
+
+	public float hpBarWidth; //hpバーの長さ
+
+	public int damage;
+
+	/// <summary>
+	/// ダメージを受けたときに減らすHPバーの量を計算
+	/// </summary>
+	/// <returns></returns>
+	private float CalcMove(float hp , int damage)
+	{
+		float temp = hp - (float)damage;
+		return hpBarWidth * (100 - temp) / 100;
+	}
+
+	private void Start()
+	{
+		//画像の横のサイズを取得
+		hpBarWidth = hpObjects[0].GetComponent<RectTransform>().sizeDelta.x;
+
+		//描画順番を指定
+		for (int i = 0; i < 5; i++)
+		{
+			hpObjects[i].transform.SetSiblingIndex(i);
+		}
+	}
+
+	private void Update()
+	{
+		hpObjects[0].GetComponent<RectTransform>().localPosition = new Vector3(CalcMove(hp , damage), 0, 0);
+	}
+}
