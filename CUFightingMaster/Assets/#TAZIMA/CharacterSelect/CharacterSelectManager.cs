ab218cd44a74fbc2d118f195d1b64ff783c8b01f
yutomisawa <yutopad0516@gmail.com>
2019-08-12 16:22:40 +0900

    キャラセレ結合

--- CUFightingMaster/Assets/#TAZIMA/CharacterSelect/CharacterSelectManager.cs
+++ CUFightingMaster/Assets/#TAZIMA/CharacterSelect/CharacterSelectManager.cs
@@ -10,19 +10,27 @@ public class CharacterSelectManager : MonoBehaviour
 	public int playerNum = 0;
 	public string controllerName = ""; //使用するコントローラーの名前
 
-	private const int maxChara = 6;
+	private const int maxChara = 4;
 	public GameObject[] character = new GameObject[maxChara];               //キャラクターゲームオブジェクト
 	public GameObject[] characterPanels = new GameObject[maxChara];    //キャラクターパネル
-	public GameObject currentSellectCharacter = null;
-	private GameObject currentSellectPanel;
-	public Vector2 inputDir = Vector2.zero;//カーソル入力方向
-	public int selectDir = 0;			//現在のカーソル位置
-	public GameObject selectCursor;		//カーソル
-	public float limitCursorFrame;		//カーソル移動
-	public float moveCursorFrames = 0;	//カーソル移動後フレーム
+    public Sprite[] characterName = new Sprite[maxChara];
 
-	void Start()
-	{
+	public GameObject currentSellectCharacter = null;
+    public Image currentSelectCharacterName;
+    public GameObject charaCreatePos;
+
+    private GameObject currentSellectPanel;
+
+    public Vector2 inputDir = Vector2.zero;//カーソル入力方向
+	public int selectDir = 0;           //現在のカーソル位置
+
+    public GameObject selectCursor;		//カーソル
+
+	public float limitCursorFrame;		//カーソル移動
+	public float moveCursorFrames = 0;  //カーソル移動後フレーム
+
+    void Start()
+    {
         //プレイヤー番号に対応した現在接続されているコントローラーを設定
         var controllerNames = Input.GetJoystickNames();
         if (playerNum < controllerNames.Length)
@@ -32,8 +40,7 @@ public class CharacterSelectManager : MonoBehaviour
                 controllerName = string.Format("{0}_", controllerNames[playerNum]);
             }
         }
-	}
-
+    }
 	void Update()
 	{
         //カーソル移動
@@ -57,34 +64,43 @@ public class CharacterSelectManager : MonoBehaviour
 	}
 	public void SelectChara(Vector2 _dir)
 	{
-		int x = selectDir % 3,y = selectDir / 3;
+        int x = selectDir;
 		//カーソルの移動を判定
 		//x軸
 		if (_dir.x == -1)
 		{
-			if (x > 0) x--;
-			else x = 2;
+			if (x > 0)
+                x--;
+			else
+                x = 3;
 		}
 		else if(_dir.x == 1)
 		{
-			if (x < 2) x++;
-			else x = 0;
+			if (x < 3)
+                x++;
+			else
+                x = 0;
 		}
-		//y軸
-		if (_dir.y == -1)
-		{
-			Debug.Log("w");
-			if (y > 0) y--;
-			else y = 1;
-		}
-		else if (_dir.y == 1)
-		{
-			Debug.Log("s");
-			if (y < 1) y ++;
-			else y = 0;
-		}
-		Debug.Log(y);
-		selectDir = x + (3 * y);
-		selectCursor.transform.position = characterPanels[selectDir].transform.position;
-	}
+		////y軸
+		//if (_dir.y == -1)
+		//{
+		//	Debug.Log("w");
+		//	if (y > 0) y--;
+		//	else y = 1;
+		//}
+		//else if (_dir.y == 1)
+		//{
+		//	Debug.Log("s");
+		//	if (y < 1) y ++;
+		//	else y = 0;
+		//}
+        selectDir = x;
+
+        //選んだ時に変わるところ
+        Destroy(currentSellectCharacter);
+       currentSelectCharacterName.sprite = characterName[selectDir];
+       currentSellectCharacter =  Instantiate(character[selectDir] , charaCreatePos.transform);
+        selectCursor.transform.position = new Vector3(characterPanels[selectDir].transform.position.x, selectCursor.transform.position.y, selectCursor.transform.position.z);
+    }
+
 }
