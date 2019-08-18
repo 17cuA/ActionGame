1cd524fdeb9058419ae10b5bbe8708645c33b251
para39 <17cu0220@jec.ac.jp>
2019-08-18 11:35:03 +0900

    コントローラーのボタン確認用スクリプト追加

---
+++ CUFightingMaster/Assets/#TAZIMA/Input/InputConfirmation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputConfirmation : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            foreach (KeyCode code in Enum.GetValues(typeof(KeyCode)))
+            {
+                if (Input.GetKeyDown(code))
+                {
+                    //処理を書く
+                    Debug.Log(code);
+                    break;
+                }
+            }
+        }
+    }
+}
