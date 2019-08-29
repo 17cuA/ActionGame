e66a9023aeb84c54c5e67e55381606e9362a2151
741PC-15\student <cu.common@gmail.com>
2019-08-29 18:50:15 +0900

    さらにリザルト変更(帰りたい)

--- CUFightingMaster/Assets/#MISAWA/Result/UI/UI_Judge.cs
+++ CUFightingMaster/Assets/#MISAWA/Result/UI/UI_Judge.cs
@@ -1,4 +1,19 @@
-﻿using System.Collections;
+﻿//---------------------------------------
+// 勝敗表示
+//---------------------------------------
+// 作成者:三沢
+// 作成日:2019.08.29
+//--------------------------------------
+// 更新履歴
+// 2019.08.29 作成
+//--------------------------------------
+// 仕様 
+// 場所を指定し、そこに勝敗を表示する
+//----------------------------------------
+// MEMO 
+//----------------------------------------
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,14 +28,19 @@ public class UI_Judge : MonoBehaviour
 		Lose = transform.Find("LOSE").gameObject;
 	}
 
-	public void Judge(int winP1cnt, int winP2cnt)
+	/// <summary>
+	/// 勝敗判定を受け取り、表示(生成に変更するよ)
+	/// </summary>
+	/// <param name="p1Judge">1Pの勝敗</param>
+	/// <param name="p2Judge">2Pの勝敗</param>
+	public void Judge(int p1Judge, int p2Judge)
 	{
-		if (winP1cnt > winP2cnt)
+		if (p1Judge > p2Judge)
 		{
 			Win.SetActive(true);
 			Lose.SetActive(false);
 		}
-		if (winP2cnt > winP1cnt)
+		if (p2Judge > p1Judge)
 		{
 			Win.SetActive(false);
 			Lose.SetActive(true);
