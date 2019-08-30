5485c60a3ea446d9e5617e0d976f8871f8e5aefc
741PC-13\student <cu.common@gmail.com>
2019-08-30 14:14:45 +0900

    Prefabフォルダの整理

--- CUFightingMaster/Assets/#TAKANO/All/General/MediaPlayer.cs
+++ CUFightingMaster/Assets/#TAKANO/All/General/MediaPlayer.cs
@@ -1,18 +1,53 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class MediaPlayer : MonoBehaviour
 {
+	public VideoPlayer videoPlayer;
+
+	private Action callBack_EndPlay;
+
+	/// <summary>
+	/// 再生
+	/// </summary>
+	public void PlayVideo()
+	{
+		//ループ終わりの時に実行するコールバックを設定
+		videoPlayer.loopPointReached += EndPlayVideo;
+		//ビデオを再生
+		videoPlayer.Play();
+	}
+
+	/// <summary>
+	/// 再生終了時に呼ばれる関数
+	/// </summary>
+	/// <returns></returns>
+	public void EndPlayVideo(VideoPlayer videoPlayer)
+	{
+		videoPlayer.isLooping = false;
+		callBack_EndPlay();
+	}
+	
+	/// <summary>
+	/// 再生終了時にコールバックする関数の登録
+	/// </summary>
+	/// <param name="action"></param>
+	public void SetEndPlayCallBack(Action action )
+	{
+		callBack_EndPlay = action;
+	}
+
     // Start is called before the first frame update
     void Start()
     {
-        
+		videoPlayer.isLooping = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
