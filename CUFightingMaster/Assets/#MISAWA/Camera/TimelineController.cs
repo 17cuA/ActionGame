9c2e4ce9d267bbd1be27c2079b8f031c9c69425e
881PC-15\student <cu.common@gmail.com>
2019-09-04 16:39:58 +0900

    inbi

---
+++ CUFightingMaster/Assets/#MISAWA/Camera/TimelineController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineController : CameraBase
+{
+	public PlayableDirector director;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public override void PlayCamera()
+    {
+        director.Play();
+    }
+}
