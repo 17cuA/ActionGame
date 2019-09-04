2d826584f118410d5d53fe23652370b437ddd4f9
741PC-15\student <cu.common@gmail.com>
2019-09-04 13:47:40 +0900

    コンボUI修正

--- CUFightingMaster/Assets/#TAZIMA/UI/ComboUIManager.cs
+++ CUFightingMaster/Assets/#TAZIMA/UI/ComboUIManager.cs
@@ -8,30 +8,67 @@ public class ComboUIManager : MonoBehaviour
 	[SerializeField] private PlayerNumber num = PlayerNumber.Player1;
 	Text text;
 	FighterCore core;
-	public Sprite combo;
-	public Sprite[] comboNum;
-	public GameObject[] comboNumSprite;
-	public GameObject comboSprite;
+	public GameObject comboObj;
+	public Sprite comboSprite;
+	public GameObject[] comboNumObj;
+	public Sprite[] comboNumSprite;
+
 
     void Start()
     {
-		text = GetComponent<Text>();
-		core = GameManager.Instance.GetPlayFighterCore(num);
+		Init();
 	}
 
     void Update()
     {
+		ComboCounter();
+	}
+	private void Init()
+	{
+		text = GetComponent<Text>();
+		core = GameManager.Instance.GetPlayFighterCore(num);
+		ResetCombo();
+	}
+	void ResetCombo()
+	{
+		for (int i = 0; i < comboNumObj.Length; i++)
+		{
+			comboNumObj[i].SetActive(false);
+		}
+		comboObj.SetActive(false);
+	}
+	public void ComboCounter()
+	{
+		var comboCount = core.ComboCount;
 		if (core.ComboCount > 1)
 		{
-			for (int i = 0; i  < core.ComboCount.ToString().Length; i++)
+			var length = comboCount.ToString("D2").Length;
+			var isCombo = new bool[length];
+			//スプライトを格納
+			for (int i = 0; i < length; i++)
 			{
-				
+				comboNumObj[i].GetComponent<Image>().sprite = comboNumSprite[comboCount / (int)Mathf.Pow(10, (i == 1) ? 0:1)];
+				isCombo[i] = true;
 			}
-			text.text = core.ComboCount.ToString() + "Combo!!!!!";
+			//表示するかどうか判定
+			for (int i = 0; i < length; i++)
+			{
+				if (comboNumObj[i].GetComponent<Image>().sprite == comboNumSprite[0]) isCombo[i] = false;
+				else break;
+			}
+			//表示する
+			for (int i = 0; i < length; i++)
+			{
+				Debug.Log(isCombo[i]);
+				comboNumObj[i].SetActive(isCombo[i]);
+			}
+			comboObj.SetActive(true);
+			//text.text = core.ComboCount.ToString() + "Combo!!!!!";
 		}
 		else
 		{
-			text.text = "";
+			ResetCombo();
+			//text.text = "";
 		}
 	}
 }
