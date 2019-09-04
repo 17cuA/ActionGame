69d656b47005758e9496b2c05fb030a8bdb8636b
881PC-18\student <cu.common@gmail.com>
2019-09-04 10:50:07 +0900

    変更

---
+++ CUFightingMaster/Assets/#TAZIMA/UI/ComboUIManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ComboUIManager : MonoBehaviour
+{
+	[SerializeField] private PlayerNumber num = PlayerNumber.Player1;
+	Text text;
+	FighterCore core;
+	public Sprite combo;
+	public Sprite[] comboNum;
+	public GameObject[] comboNumSprite;
+	public GameObject comboSprite;
+
+    void Start()
+    {
+		text = GetComponent<Text>();
+		core = GameManager.Instance.GetPlayFighterCore(num);
+	}
+
+    void Update()
+    {
+		if (core.ComboCount > 1)
+		{
+			for (int i = 0; i  < core.ComboCount.ToString().Length; i++)
+			{
+				
+			}
+			text.text = core.ComboCount.ToString() + "Combo!!!!!";
+		}
+		else
+		{
+			text.text = "";
+		}
+	}
+}
