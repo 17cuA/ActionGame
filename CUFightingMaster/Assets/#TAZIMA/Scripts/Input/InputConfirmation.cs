201100854bf6f8205986d97f0e8ab7bb1c0c0d1d
741PC-25\student <cu.common@gmail.com>
2019-09-06 11:56:36 +0900

    InputManager整理

--- CUFightingMaster/Assets/#TAZIMA/Scripts/Input/InputConfirmation.cs
+++ CUFightingMaster/Assets/#TAZIMA/Scripts/Input/InputConfirmation.cs
@@ -2,15 +2,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InputConfirmation : MonoBehaviour
 {
-    // Update is called once per frame
     void Update()
     {
-        if (Input.anyKeyDown)
+		if (Input.anyKeyDown)
         {
-            foreach (KeyCode code in Enum.GetValues(typeof(KeyCode)))
+			var names = Input.GetJoystickNames();
+			Debug.Log(names.Length);
+			var sampleNames = new string[names.Length];
+			for (int i = 0;i < names.Length;i++)
+			{
+				Debug.Log(names[i]);
+			}
+			foreach (KeyCode code in Enum.GetValues(typeof(KeyCode)))
             {
                 if (Input.GetKeyDown(code))
                 {
