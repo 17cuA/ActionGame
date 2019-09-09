a6352ebdbcb10c81d104a1691e78296733b8bc13
Masa0809 <honaga.masaki@gmail.com>
2019-09-10 01:18:19 +0900

    高野へ

--- CUFightingMaster/Assets/#HONAGA/Scripts/SceneChange_Manager.cs
+++ CUFightingMaster/Assets/#HONAGA/Scripts/SceneChange_Manager.cs
@@ -7,15 +7,17 @@ using UnityEngine.SceneManagement;
 
 public class SceneChange_Manager : MonoBehaviour
 {
+    public CanvasController_CharacterSelect canvasController_CharacterSelect;
     public CharacterSelect_Manager Manager;
     public Image Canvas_One;
     public Image Canvas_Two;
 
-	[SerializeField]
+    [SerializeField]
     private AsyncOperation async;
     // Start is called before the first frame update
     void Start()
     {
+        canvasController_CharacterSelect.InitDownCurtain();
         Manager = GameObject.Find("Manager").GetComponent<CharacterSelect_Manager>();
         async = SceneManager.LoadSceneAsync("Battle");
         async.allowSceneActivation = false;
@@ -24,12 +26,15 @@ public class SceneChange_Manager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Manager.sceneChangeJughe == true && Manager.FadeFrame >= 1.0f)
+        if (canvasController_CharacterSelect.UpCurtain())
         {
-            Canvas_One.color += new Color(0.0f,0.0f,0.0f,0.02f);
-            Canvas_Two.color += new Color(0.0f, 0.0f, 0.0f, 0.02f);
+            if (Manager.sceneChangeJughe == true && Manager.FadeFrame >= 1.0f)
+            {
+                Canvas_One.color += new Color(0.0f, 0.0f, 0.0f, 0.02f);
+                Canvas_Two.color += new Color(0.0f, 0.0f, 0.0f, 0.02f);
+            }
         }
-        if (Canvas_One.color.a >= 1.0f && Canvas_Two.color.a >= 1.0f)
+        if (Canvas_One.color.a >= 1.0f && Canvas_Two.color.a >= 1.0f && canvasController_CharacterSelect.DownCurtain())
         {
             async.allowSceneActivation = true;
         }
