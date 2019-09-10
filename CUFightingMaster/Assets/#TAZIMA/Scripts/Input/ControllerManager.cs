40a5b9c7fcbf51878407dcf008550c58dce1fd67
741PC-24\student <cu.common@gmail.com>
2019-09-10 18:48:16 +0900

    コントローラーマネージャ変更

--- CUFightingMaster/Assets/#TAZIMA/Scripts/Input/ControllerManager.cs
+++ CUFightingMaster/Assets/#TAZIMA/Scripts/Input/ControllerManager.cs
@@ -10,6 +10,7 @@ public class ControllerManager : MonoBehaviour
     [SerializeField] private const int playerNum = 2;               //プレイヤー数
     public GameObject[] inputManager = new GameObject[playerNum];   //InputManagerオブジェクトを管理
     public string[] controllerNames = new string[playerNum];        //コントローラーの名前を管理
+	private int[] controllerIndex = new int[playerNum];				//接続されているコントローラーの要素数を保存しておく
 
     /// <summary>
     /// 初期化処理
@@ -25,19 +26,28 @@ public class ControllerManager : MonoBehaviour
     void Update()
     {
         var nowControllerNames = Input.GetJoystickNames();
+		var isSet = new bool[playerNum];//変更した場合trueにする
         for (int i = 0;i < playerNum; i++)
         {
-            if ( i < nowControllerNames.Length)
-            {
-                //変更が必要かどうか
-				if (inputManager[i].GetComponent<TestInput>().controllerName != nowControllerNames[i] + "_")
+			isSet[i] = false;
+			if (inputManager[i].GetComponent<TestInput>().controllerName != nowControllerNames[controllerIndex[i]])
+			{
+				for (int j = 0; j < nowControllerNames.Length; j++)
 				{
-					if (nowControllerNames[i] != "")
-						inputManager[i].GetComponent<TestInput>().controllerName = nowControllerNames[i] + "_";
-					else
-						inputManager[i].GetComponent<TestInput>().controllerName = nowControllerNames[i];
+					//現在のコントローラーの要素数をプレイヤー数で割った数が現在設定しているプレイヤーの要素数でなければループ
+					if (j % playerNum != i) continue;
+					//コントローラーが接続されていればそのコントローラーの名前に変更する
+					if (nowControllerNames[j] != "")
+					{
+						inputManager[i].GetComponent<TestInput>().controllerName = nowControllerNames[j] + "_";
+						isSet[i] = true;
+					}
+					//変更された場合このループのその後の処理を無視する
+					if (isSet[i]) break;
 				}
-            }
+				//変更が必要なのに変更されていなかった場合コントローラー名を空にする
+				if (!isSet[i])	inputManager[i].GetComponent<TestInput>().controllerName = "";
+			}
         }
     }
 }
