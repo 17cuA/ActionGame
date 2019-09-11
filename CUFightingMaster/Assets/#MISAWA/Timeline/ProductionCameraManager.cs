56b88ae0e17095e2a087c4229028a711cd43151b
kazuma <17cu0235@gmail.com>
2019-09-12 03:10:09 +0900

    勝利リザルト

--- CUFightingMaster/Assets/#MISAWA/Timeline/ProductionCameraManager.cs
+++ CUFightingMaster/Assets/#MISAWA/Timeline/ProductionCameraManager.cs
@@ -8,4 +8,6 @@ public class ProductionCameraManager : SingletonMono<ProductionCameraManager>
 {
 	// CameraのCinemachineBrainを保存
 	public CinemachineBrain[] cinemachine = new CinemachineBrain[2];
+    public Camera player1Camera = null;
+    public Camera player2Camera = null;
 }
