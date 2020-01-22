5dbf6dcd25884133d582dbadbb9da5b9bf584032
741PC-33\student <cu.common@gmail.com>
2020-01-22 12:04:08 +0900

    ゴリラプログラミング

--- CUFightingMaster/Assets/#KANAZAWA/Editor/AnimationClipProcessor.cs
+++ CUFightingMaster/Assets/#KANAZAWA/Editor/AnimationClipProcessor.cs
@@ -5,6 +5,14 @@ using System.Collections;
 
 public class AnimationClipProcessor : AssetPostprocessor
 {
+    private enum eAnimationClipInfo
+    {
+        name,
+        firstFrame,
+        lastFrame,
+        isLoop,
+    }
+
     /// <summary>
     /// モデルのImportSettingを設定する
     /// </summary>
@@ -12,12 +20,38 @@ public class AnimationClipProcessor : AssetPostprocessor
     static void SetModelImportSettings(ModelImporter _importer)
     {
         ArrayList list = new ArrayList();
-        ModelImporterClipAnimation clip = new ModelImporterClipAnimation();
-        clip.firstFrame = 1;
-        clip.lastFrame = 60;
-        list.Add(clip);
-        // 引数のオブジェクトのClipAnimationを変更
-        _importer.clipAnimations = (ModelImporterClipAnimation[])
+        TextLoader text = new TextLoader();
+        text.LoadText("AnimationClipList");
+        for (int i = 0; i < text.textWords.GetLength(0); i++)
+        {
+            ModelImporterClipAnimation clip = new ModelImporterClipAnimation();
+
+            for (int j = 0; j < text.textWords.GetLength(1); j++)
+            {
+                switch(j)
+                {
+                    case (int)eAnimationClipInfo.name:
+                        clip.name = text.textWords[i, j];
+                        break;
+                    case (int)eAnimationClipInfo.firstFrame:
+                        clip.firstFrame = int.Parse(text.textWords[i, j]);
+                        break;
+                    case (int)eAnimationClipInfo.lastFrame:
+                        clip.lastFrame = int.Parse(text.textWords[i, j]);
+                        break;
+                    case (int)eAnimationClipInfo.isLoop:
+                        if (text.textWords[i, j] == null)
+                        {
+                            clip.loop = false;
+                        }
+                        else clip.loop = true;
+                        break;
+                }
+            }
+            list.Add(clip);
+        }
+            // 引数のオブジェクトのClipAnimationを変更
+            _importer.clipAnimations = (ModelImporterClipAnimation[])
             // リストを配列(ClipAnimation)に変更
             list.ToArray(typeof(ModelImporterClipAnimation));
 
---
+++ CUFightingMaster/Assets/#KANAZAWA/Editor/TextLoader.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextLoader : MonoBehaviour
+{
+    public string[,] textWords;
+
+    private string[] textMessage;
+    private int rowLength;
+    private int columnLength;
+
+    public void LoadText(string fileName)
+    {
+        TextAsset textAsset = new TextAsset();
+        textAsset = Resources.Load<TextAsset>("Animation/" + fileName);
+        if (textAsset == null)
+        {
+            Debug.LogError("テキストファイルが見つかりませんでした。");
+            return;
+        }
+
+        // 取得したファイルを改行で分割
+        textMessage = textAsset.text.Split('\n');
+        // 改行した数から列数を取得
+        rowLength = textMessage.Length;
+        // 一行目のテキストをタブで分割し行数を取得
+        columnLength = textMessage[0].Split(',').Length;
+        textWords = new string[rowLength, columnLength];
+
+        for(int i = 0; i < rowLength; i++)
+        {
+            string[] textLine = textMessage[i].Split(',');
+            for (int j = 0; j < columnLength; j++)
+            {
+                textWords[i, j] = textLine[j];
+            }
+        }
+    }
+
+}
