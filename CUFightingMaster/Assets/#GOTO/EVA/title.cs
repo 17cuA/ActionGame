906ee587f307bb1facfe4306e7c225290bf1b6b6
741PC-39\student <cu.common@gmail.com>
2020-02-26 11:49:05 +0900

    Evaデータ追加

---
+++ CUFightingMaster/Assets/#GOTO/EVA/title.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class title : MonoBehaviour
+{
+
+	public VideoPlayer videoPlayer;  //アタッチした VideoPlayer をインスペクタでセットする
+	public VideoPlayer videoPlayer1;  //アタッチした VideoPlayer をインスペクタでセットする
+
+	private void Start()
+	{
+		Sound.LoadBGM("BGM_Title", "BGM_Title");
+		Sound.StopBGM();
+	}
+	// Update is called once per frame
+	void Update()
+	{
+		Debug.Log(videoPlayer.frame);
+		Debug.Log(videoPlayer.frameCount);
+		if ((ulong)videoPlayer.frame == videoPlayer.frameCount - 2)
+		{ 
+			//※ここに終了したときの処理など
+			SceneManager.LoadScene("EVABattle");
+		}
+	}
+}
